d996978b6fbbdddc41b05fed51ad0709f5c54e07
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-23 23:04:38 -0600

    Defined matrix for backtracking algorithm and vert. and horz. num. check

--- solver.py
+++ solver.py
@@ -1,13 +1,44 @@
-import numpy as np
+# import numpy as np
+import math.floor as floor
 
 # solve function
-    # verify dimensions
+def solve(board):
+
     # count n empty tiles
-    # define n-by-9 boolean array in zeros
+    emptyTiles = 0
+    for iRow in range(9):
+        for jCol in range(9):
+            if board[iRow][jCol] == 0:
+                emptyTiles += 1
+
+    # define n-by-9 boolean array
+    isValid = [[True] * 9] * emptyTiles
+
     # while index < n
+    kTile = 0
+    pRow = 0
+    qCol = 0
+    while kTile < emptyTiles:
+
         # find empty tile
-        # store tile index (1 through n)
-        # store valid answers in corresponding row (ascending order)
+        isFound = False
+        while ~isFound:
+            if board[pRow][qCol] == 0:
+                isFound = True
+            qCol += 1
+            if qCol == 9:
+                pRow += 1
+                qCol = 0
+
+        # store invalid answers in corresponding row (ascending order)
+        for sCol in range(9):
+            if board[pRow][sCol] ~= 0:
+                isValid[kTile][board[pRow][sCol] - 1] = False
+        for rRow in range(9):
+            if board[rRow][qCol] ~= 0:
+                isValid[kTile][board[rRow][qCol] - 1] = False
+        ## here goes the check for the local 3-by-3 square
+
         # if there are no valid answers:
             # index -= 1
             # if index = 0:
@@ -16,4 +47,17 @@ import numpy as np
         # set empty tile to first valid answer and set corresponding boolean to 0
         # index += 1
 
-# note: empty tiles are those containing any other number than 1 through 9
+# note: empty tiles contain 0
+
+testBoard = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,8,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+solve(testBoard)
